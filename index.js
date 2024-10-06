367220143286682dfc251008d510602cea9a8faf
hasanrahat <rahat.ruet@gmail.com>
2024-10-06 19:29:40 +0600

    Update-final-code

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ app.get('/courses',(req,res)=>{
     res.send(courses);
    });
 
+app.get('/courses/:id',(req,res)=>{
+    const id = req.params.id; // Get id from URL and convert to a number
+    const selectedCourse = courses.find((course) => course._id === id);
+    res.send(selectedCourse);
+   });  
+
 app.listen(port,()=>{
     console.log(`Server is running on ${port}`);
 });
